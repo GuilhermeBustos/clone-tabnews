1fdcaf58a8eff4f995ddc0778682d0efcb8950ec
Guilherme Bustos <guilherme.bustos@outlook.com>
2026-03-09 17:21:22 -0300

    refactor: use `next-connect` in `/migrations`, `/status` and adds `PUT` validation in `post.test.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,50 +1,46 @@
+import { createRouter } from "next-connect";
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database.js";
+import controller from "infra/controller.js";
 
-export default async function migrations(request, response) {
-  const allowedRequestMethods = ["GET", "POST"];
+const router = createRouter();
 
-  if (!allowedRequestMethods.includes(request.method)) {
-    return response.status(405).json({
-      error: `Method ${request.method} not allowed.`,
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const pendingMigrations = await database.withClient(async (dbClient) => {
+    return migrationRunner(createMigrationOptions(dbClient));
+  });
+
+  return response.status(200).json(pendingMigrations);
+}
+
+async function postHandler(request, response) {
+  const migratedMigrations = await database.withClient(async (dbClient) => {
+    return migrationRunner({
+      ...createMigrationOptions(dbClient),
+      dryRun: false,
     });
+  });
+
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
 
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
+  return response.status(200).json(migratedMigrations);
+}
 
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
-
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      return response.status(200).json(pendingMigrations);
-    }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-
-      return response.status(200).json(migratedMigrations);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await dbClient.end();
-  }
+function createMigrationOptions(dbClient) {
+  return {
+    dbClient,
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -18,3 +18,23 @@ describe("GET /api/v1/migrations", () => {
     });
   });
 });
+
+describe("PUT /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations with wrong method", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowed",
+        message: "Method not allowed",
+        action: "Check if the requested method is correct",
+        status_code: 405,
+      });
+    });
+  });
+});
