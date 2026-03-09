48f21d26f08ff9b406fcc30333f026566d05d538
Guilherme Bustos <guilherme.bustos@outlook.com>
2026-03-09 17:14:43 -0300

    feat: adds `InternalServerError`, `ServiceError` and `MethodNotAllowedError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,49 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("An unexpected internal error occurred.", {
       cause,
     });
     this.name = "InternalServerError";
     this.action = "Contact the support team";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service unavailable", {
+      cause,
+    });
+    this.name = "ServiceError";
+    this.action = "Check if the requested service is available";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Method not allowed");
+    this.name = "MethodNotAllowed";
+    this.action = "Check if the requested method is correct";
+    this.statusCode = 405;
   }
 
   toJSON() {
