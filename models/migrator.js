035903fff8e1b19e9468cdcd8a71590b24e81f9e
Guilherme Bustos <guilherme.bustos@outlook.com>
2026-03-10 10:06:56 -0300

    feat: add `model/migrator.js`

---
+++ models/migrator.js
@@ -0,0 +1,40 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+
+function createMigrationOptions(dbClient) {
+  return {
+    dbClient,
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+}
+
+async function listPendingMigrations() {
+  const pendingMigrations = await database.withClient(async (dbClient) => {
+    return migrationRunner(createMigrationOptions(dbClient));
+  });
+
+  return pendingMigrations;
+}
+
+async function runPendingMigrations() {
+  const migratedMigrations = await database.withClient(async (dbClient) => {
+    return migrationRunner({
+      ...createMigrationOptions(dbClient),
+      dryRun: false,
+    });
+  });
+
+  return migratedMigrations;
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
