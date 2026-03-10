67a1bbf42e8e071677c5db0e5764e3f3500c2966
Guilherme Bustos <guilherme.bustos@outlook.com>
2026-03-10 11:36:18 -0300

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -8,7 +8,7 @@ function createMigrationOptions(dbClient) {
     dryRun: true,
     dir: resolve("infra", "migrations"),
     direction: "up",
-    verbose: true,
+    log: () => {},
     migrationsTable: "pgmigrations",
   };
 }
